ec1c9bf9e1bf47876f9a2f8e56c41ae1871be07f
Gabriel Adomnicai <gadomnicai@linkedin.com>
2016-08-15 17:16:00 -0700

    Simple server fixes

--- app/simpleserver.hs
+++ app/simpleserver.hs
@@ -1,7 +1,12 @@
 -- | A simple server
 module Main (main) where
 
+import Control.Concurrent
+import Control.Monad
+import Network.Socket
+import Network (listenOn,PortID(..))
 import System.IO
+import Text.Printf
 
 talk :: Handle -> IO ()
 talk h =
@@ -13,4 +18,13 @@ talk h =
                 then hPutStrLn h "Thank you for using the doubling service"
                 else hPrint h (2 * (read line :: Integer)) >> loop
 
-main = undefined
+port :: Int 
+port = 44444
+
+main = withSocketsDo $ do
+  sock <- listenOn $ PortNumber (fromIntegral port)
+  printf "Listening on port %d\n" port
+  forever $ do
+    (handle,host,port) <- accept sock
+    printf "Accepted connection from %s: %s\n" host (show port)
+    forkFinally (talk handle) (const $ hClose handle)
